e7a587ab9eceb95362577cd4044c93b936912e83
sheikhsadi46 <sheikhsadiemon01@gmail.com>
2022-10-31 21:03:09 +0600

    Demo APP

---
+++ demo/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Demo Bank</a>
+      </header>
+      <main>Beneficairy</main>
+    </div>
+  );
+}
+
+export default App;
